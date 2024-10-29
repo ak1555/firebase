08aecfa9b784b2e65269a03ee04e0bd533df80c1
abinkrishna <abin48515@gmail.com>
2024-10-29 17:55:27 +0530

    save

---
+++ loginpage/lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:loginpage/loginpage.dart';
+import 'package:loginpage/signuppage.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: SignupPage(),
+    )
+  );
+}
